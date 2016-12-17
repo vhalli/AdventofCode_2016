58ea4005439aee1cc32564d75508792c8bf07143
Venkatesh Halli <venkatesh.fatality@gmail.com>
2016-12-17 13:49:11 +0530

    Bunnies can now locate HQ.

---
+++ day1/day1.ml
@@ -0,0 +1,37 @@
+type heading = North | South | East | West
+type direction = Left | Right
+type instruction = direction * int
+type position = {heading : heading;
+                 x : int;
+                 y : int}
+
+let initial_pos = {heading = North;
+                   x = 0;
+                   y = 0}
+
+let turn dirn hdg : heading = 
+  match dirn with
+  | Right -> (match hdg with
+      | North -> East
+      | East -> South
+      | South -> West
+      | West -> North)
+  | Left -> (match hdg with
+      | North -> West
+      | West -> South
+      | South -> East
+      | East -> North)
+
+let step pos dist : position = 
+  match pos.heading with
+  | North -> {pos with y = pos.y + dist}
+  | South -> {pos with y = pos.y - dist}
+  | East -> {pos with x = pos.x + dist}
+  | West -> {pos with x = pos.x - dist}
+
+let walk pos (dirn, dist) : position =
+  let newhdg = turn dirn pos.heading in
+  step {pos with heading = newhdg} dist
+
+let findHQ instructions =
+  List.fold_left walk initial_pos instructions
